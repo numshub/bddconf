93e0b2970c07f9a30eb5bd99734bc9ad4f81f660
Pierpaolo Da Fieno <pierpaolo.dafieno@gmail.com>
2017-03-24 13:51:49 +0100

    Attribute decomposition implemented

--- bddconf/bddconf.py
+++ bddconf/bddconf.py
@@ -3,6 +3,7 @@
 from typing import List
 from collections import OrderedDict
 import openpyxl
+import math
 
 
 class Attribute:
@@ -12,16 +13,57 @@ class Attribute:
         self.name = name
         self.values = values
 
-    def size(self):
+    def len(self):
         return len(self.values)
 
-    def nbit(self):
-        return len(bin(self.size())) - 2
+    def size(self, base=2):
+        return math.floor(math.log(self.len() - 1, base)) + 1
+
+    def decompose(self) -> (OrderedDict, List):
+        """
+
+        Returns:
+            (mapping, rules)
+
+            mapping: OrderedDict "attribute.code:attribute.values.get(value)" --> (var1, val1), (var2, val2)
+            invalid_values: List
+
+        """
+        vars = tuple("{}_{:06d}".format(self.code, n)
+               for n in range(self.size()))
+        mapping = OrderedDict()
+        invalid_values = []
+        for n, val in enumerate(self.values):
+            tag = "{code}:{n:0{width}d}".format(code=self.code,
+                                                n=n, width=self.size(base=10))
+            values = self._binary_representation(n)
+            mapping[tag] = tuple(zip(vars, values))
+        # Mark the unused value as invalid
+        # Get the max used value
+        max_used_val = self.len() - 1
+        # Get the max possible value according to bit size
+        max_val = 2 ** self.size() - 1
+        for n in range(max_used_val + 1, max_val + 1):
+            invalid_value = []
+            for n, k in enumerate(self._binary_representation(n)):
+                atom=vars[n]
+                if not k:
+                    atom = "!{}".format(atom)
+                invalid_value.append(atom)
+            invalid_value = " & ".join(invalid_value)
+            invalid_values.append(invalid_value)
+        return mapping, invalid_values
+
+    def _binary_representation(self, n, width=None):
+        if width is None:
+            width = self.size()
+        return tuple(map(int, "{n:0{width}b}".format(n=n, width=width)))
 
     def __repr__(self):
-        return "{code}-{name}[{size:03d}]".format(code=self.code,
-                                                  name=self.name,
-                                                  size=self.size())
+        return "{code}-{name}[{size:{width}d}]".format(code=self.code,
+                                                       name=self.name,
+                                                       size=self.len(),
+                                                       width=self.size(base=10))
 
 
 class ConfigBDD:
@@ -30,22 +72,23 @@ class ConfigBDD:
         self.attributes = []
         self.variables = []
         self.mapping = None
-        self.rules = None
+        self.invalid_values = None
 
     def append_attribute(self, attribute: Attribute):
         self.attributes.append(attribute)
 
     def decompose(self):
-        """Decompose in binary variables
+        """Decomposes an attribute in binary variables
         """
+        mapping_dict = OrderedDict()
+        invalid_values = []
         for attr in self.attributes:
-            variables = ["{}_{:06d}".format(attr.code,
-                                            n) for n in range(attr.nbit())]
-            mapping = OrderedDict()
-            for n, val in enumerate(attr.values):
-                tag = "{code}:{n:03}".format(code=attr.code,
-                                             n=n)
-
+            attr_mapping, invalid_attr_values = attr.decompose()
+            mapping_dict.update(attr_mapping)
+            if invalid_attr_values:
+                invalid_values.extend(invalid_attr_values)
+        self.mapping = mapping_dict
+        self.invalid_values = invalid_values
 
     def parse_workbook(self, filename: str):
         ws = openpyxl.load_workbook(filename).worksheets[0]
--- tests/test_bddconf.py
+++ tests/test_bddconf.py
@@ -16,14 +16,29 @@ import openpyxl
 
 INFILE = "tests/test_shirts.xlsx"
 
-
-def test_parse_workbook():
+@pytest.fixture()
+def configuration():
     config = bddconf.ConfigBDD()
     config.parse_workbook(INFILE)
-    for attr in config.attributes:
-        print(attr)
-    assert False
+    return config
 
 
-def test_parse_workbook():
-    assert True
+def test_parse_workbook(configuration):
+    a, b, c  = configuration.attributes
+    assert a.name == "Size"
+    assert b.name == "Color"
+    assert c.name == "Sex"
+    assert a.code == "A001"
+    assert b.code == "A002"
+    assert c.code == "A003"
+    assert a.len() == 6
+    assert b.len() == 4
+    assert c.len() == 2
+    assert a.size() == 3
+    assert b.size() == 2
+    assert c.size() == 1
+
+def test_decompose(configuration):
+    a, b, c  = configuration.attributes
+    configuration.decompose()
+    pass
